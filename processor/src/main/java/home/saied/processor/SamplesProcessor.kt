a3579a9dcfd0aeda1ff6e9040cdff4846a7af1a0
Saeed Momenzadeh <saied.momen@balad.ir>
2021-12-22 21:48:04 +0330

    add read sample source read

--- processor/src/main/java/home/saied/processor/SamplesProcessor.kt
+++ processor/src/main/java/home/saied/processor/SamplesProcessor.kt
@@ -1,10 +1,13 @@
 package home.saied.processor
 
 import com.google.devtools.ksp.processing.*
+import com.google.devtools.ksp.symbol.FileLocation
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSDeclaration
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import com.squareup.kotlinpoet.ksp.KotlinPoetKspPreview
 import com.squareup.kotlinpoet.ksp.writeTo
+import java.io.File
 
 class SamplesProcessor(val codeGenerator: CodeGenerator, val logger: KSPLogger) : SymbolProcessor {
 
@@ -12,10 +15,32 @@ class SamplesProcessor(val codeGenerator: CodeGenerator, val logger: KSPLogger) 
         resolver.getAllFiles().filter { file ->
 //            logger.warn("sample file:${file.filePath}")
             file.declarations.any(::isSampledComposable)
-        }.forEach {
-            logger.warn("sample file:", it)
+        }.toList().forEach { file ->
+            val toList = file.declarations.filter(::isSampledComposable)
+            logger.warn("file:${file.fileName}")
+            toList.forEach {
+                logger.warn("  " + it.simpleName.asString() + ":" + readDeclarationSourceCode(it))
+            }
         }
         return emptyList()
+    }
+
+    private fun readDeclarationSourceCode(declaration: KSDeclaration): String {
+        check(declaration is KSFunctionDeclaration)
+        return declaration.containingFile!!.let {
+            val declarationLineNumber = (declaration.location as FileLocation).lineNumber
+            File(it.filePath).useLines { lines ->
+                val lineList = lines.toList()
+                buildString {
+                    var i = declarationLineNumber - 1 // Include function signature
+                    while (lineList[i] != "}") {
+                        appendLine(lineList[i])
+                        i++
+                    }
+                    appendLine("}")
+                }
+            }
+        }
     }
 
     private fun isSampledComposable(declaration: KSDeclaration): Boolean {
