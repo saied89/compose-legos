52b4be4f215244de3ca9e710fb1fe5060de2276e
Saeed Momenzadeh <saied.momen@balad.ir>
2021-12-24 19:01:05 +0330

    add switch between code and composable

--- app/src/main/java/home/saied/composesamples/ui/Sample.kt
+++ app/src/main/java/home/saied/composesamples/ui/Sample.kt
@@ -1,10 +1,60 @@
 package home.saied.composesamples.ui
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.Crossfade
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import home.saied.samples.Sample
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SampleView(sample: Sample) {
-    Text(text = sample.body)
+    var sampleViewSwitchState: SampleViewSwitch by remember {
+        mutableStateOf(SampleViewSwitch.SOURCE)
+    }
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                sampleViewSwitchState = when (sampleViewSwitchState) {
+                    SampleViewSwitch.SOURCE -> {
+                        SampleViewSwitch.COMPOSABLE
+                    }
+                    SampleViewSwitch.COMPOSABLE -> {
+                        SampleViewSwitch.SOURCE
+                    }
+                }
+            }) {
+                Icon(
+                    imageVector = when (sampleViewSwitchState) {
+                        SampleViewSwitch.SOURCE -> {
+                            Icons.Default.PlayArrow
+                        }
+                        SampleViewSwitch.COMPOSABLE -> {
+                            Icons.Default.Code
+                        }
+                    },
+                    contentDescription = null
+                )
+            }
+        }
+    ) {
+        Crossfade(targetState = sampleViewSwitchState) {
+            when (it) {
+                SampleViewSwitch.SOURCE -> {
+                    Text(text = sample.body)
+                }
+                SampleViewSwitch.COMPOSABLE -> {
+                    sample.block()
+                }
+            }
+        }
+
+    }
+}
+
+enum class SampleViewSwitch {
+    SOURCE, COMPOSABLE
 }
