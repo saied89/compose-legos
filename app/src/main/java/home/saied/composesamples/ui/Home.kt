ee665aac53f7553805897c23c1f6eb87c9182e82
Saeed Momenzadeh <saied.momen@balad.ir>
2021-12-29 00:42:00 +0330

    add search field clear focus

--- app/src/main/java/home/saied/composesamples/ui/Home.kt
+++ app/src/main/java/home/saied/composesamples/ui/Home.kt
@@ -1,24 +1,27 @@
 package home.saied.composesamples.ui
 
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import home.saied.samples.SampleModule
 
@@ -26,30 +29,56 @@ import home.saied.samples.SampleModule
 @Composable
 fun HomeScreen(moduleList: List<SampleModule>, onModuleClick: (Int) -> Unit) {
 
-    var homeState by remember { mutableStateOf(HomeState.MODULES) }
+    val searchBoxInteractionSource = remember { MutableInteractionSource() }
+    val searchIsPressed by searchBoxInteractionSource.collectIsFocusedAsState()
+    val homeState =
+        if (searchIsPressed)
+            HomeState.SEARCH
+        else HomeState.MODULES
+
 
     val searchTransition = updateTransition(homeState, "SearchTransition")
 
-    searchTransition.AnimatedContent() {
+    val searchCornerDp by searchTransition.animateDp(label = "searchCornerDp") {
         when (it) {
-            HomeState.MODULES -> {
-                Column {
-                        OutlinedTextField(
-                            value = "",
-                            onValueChange = {},
-                            enabled = false,
-                            placeholder = { Text(text = "Search Samples") },
-                            leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
-                            textStyle = MaterialTheme.typography.titleSmall,
-                            shape = RoundedCornerShape(12.dp),
-                            colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = Color.LightGray),
-                            modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                        )
-                    ModuleList(moduleList = moduleList, onModuleClick = onModuleClick)
-                }
-            }
-            HomeState.SEARCH -> {}
+            HomeState.MODULES -> 12.dp
+            HomeState.SEARCH -> 0.dp
         }
+    }
+
+    val seachPaddingDp by searchTransition.animateDp(label = "searchCornerDp") {
+        when (it) {
+            HomeState.MODULES -> 16.dp
+            HomeState.SEARCH -> 0.dp
+        }
+    }
+
+    val focusManager = LocalFocusManager.current
+    Column {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            interactionSource = searchBoxInteractionSource,
+            placeholder = { Text(text = "Search Samples") },
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        focusManager.clearFocus()
+                    })
+            },
+            textStyle = MaterialTheme.typography.titleSmall,
+            shape = RoundedCornerShape(searchCornerDp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                backgroundColor = Color.LightGray,
+                focusedBorderColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .padding(seachPaddingDp)
+                .fillMaxWidth()
+        )
+        ModuleList(moduleList = moduleList, onModuleClick = onModuleClick)
     }
 
 }
