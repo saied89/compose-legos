48e21bb7744062746f8d3fa2678db60ee8fdbc5d
Saeed Momenzadeh <saied.momen@balad.ir>
2022-01-01 14:29:03 +0330

    add gutter to code view

--- app/src/main/java/home/saied/composesamples/ui/SampleSceen.kt
+++ app/src/main/java/home/saied/composesamples/ui/SampleSceen.kt
@@ -1,15 +1,17 @@
 package home.saied.composesamples.ui
 
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Code
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import home.saied.samples.Sample
 
@@ -60,9 +62,68 @@ fun SampleScreen(sample: Sample) {
 }
 
 @Composable
-fun Code(code: String) {
-    Text(text = code, modifier = Modifier.padding(16.dp))
+private fun Code(code: String) {
+    val lines = remember {
+        code.lines()
+    }
+    val gutterWidth = lines.size.toString().length
+    Column {
+        lines.forEachIndexed { index, line ->
+            CodeLine(index = index, codeLine = line, gutterWidth = gutterWidth)
+        }
+    }
 }
+
+@Composable
+private fun CodeLine(index: Int, codeLine: String, gutterWidth: Int) {
+    Row(modifier = Modifier.height(IntrinsicSize.Min)) {
+        Text(
+            text = index.toString().padStart(gutterWidth, '0'),
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier
+                .padding(2.dp)
+                .align(CenterVertically)
+        )
+        Box(
+            modifier = Modifier
+                .width(1.dp)
+                .fillMaxHeight()
+                .background(Color.DarkGray)
+        )
+        Text(
+            text = codeLine,
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.align(CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CodeLinePreview() {
+    CodeLine(index = 0, codeLine = "test", 1)
+}
+
+
+//@Composable
+//@Preview
+//fun Preview() {
+//    Code(code = """
+//    |fun BasicTextFieldSample() {
+//    |    var value by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+//    |        mutableStateOf(TextFieldValue())
+//    |    }
+//    |    BasicTextField(
+//    |        value = value,
+//    |        onValueChange = {
+//    |            // it is crucial that the update is fed back into BasicTextField in order to
+//    |            // see updates on the text
+//    |            value = it
+//    |        }
+//    |    )
+//    |}
+//    |""")
+//}
 
 enum class SampleViewSwitch {
     SOURCE, COMPOSABLE
