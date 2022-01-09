a502ee26143c1105fc32a81c451084eb461e265f
Saeed Momenzadeh <saied.momen@balad.ir>
2022-01-09 23:38:16 +0330

    add title and item icons to files

--- app/src/main/java/home/saied/composesamples/ui/ModuleScreen.kt
+++ app/src/main/java/home/saied/composesamples/ui/ModuleScreen.kt
@@ -4,14 +4,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import home.saied.composesamples.R
 import home.saied.samples.SampleModule
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -19,16 +26,44 @@ import home.saied.samples.SampleModule
 fun ModuleScreen(sampleModule: SampleModule, onFileClick: (Int) -> Unit) {
     LazyColumn {
         item {
-            Spacer(modifier = Modifier.height(32.dp))
+            val titleAnnotatedString = with(AnnotatedString.Builder()) {
+                append("List of ")
+                pushStyle(SpanStyle(fontWeight = FontWeight.Medium, fontStyle = FontStyle.Italic))
+                append(sampleModule.name)
+                pop()
+                append(" sample files")
+                toAnnotatedString()
+            }
+            Text(
+                text = titleAnnotatedString,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(16.dp)
+            )
         }
         itemsIndexed(sampleModule.sampleFileList, itemContent = { index, item ->
             ListItem(
                 text = { Text(text = item.name) },
-                modifier = Modifier.padding(horizontal = 16.dp).clickable {
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_compose_file_concept),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                        modifier = Modifier.size(48.dp)
+                    )
+                },
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .clickable {
                         onFileClick(index)
-                }
+                    }
             )
             Spacer(modifier = Modifier.height(8.dp))
         })
     }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    ModuleScreen(sampleModule = SampleModule("animation-core", "", listOf()), onFileClick = {})
 }
