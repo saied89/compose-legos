d0d0eb2e98a006a0c4b7b4f6f8e9531f783c6d97
saied89 <saiedmomen@gmail.com>
2022-06-14 22:37:14 +0430

    update debs

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -68,9 +68,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.6.0")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
     implementation( "androidx.compose.ui:ui:$compose_version")
     implementation("androidx.compose.material:material:$compose_version")
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
@@ -82,6 +82,7 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:$compose_version")
     implementation ("androidx.activity:activity-compose:1.4.0")
     implementation("androidx.compose.material3:material3:1.0.0-alpha11")
+    compileOnly("")
     ksp(project(":processor"))
 
     testImplementation("junit:junit:4.13.2")
