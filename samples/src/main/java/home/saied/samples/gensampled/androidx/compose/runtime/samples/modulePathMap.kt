afde09737d2c66ba9e91b425829b83927862fb45
saied89 <saiedmomen@gmail.com>
2022-08-26 11:14:37 +0430

    add module path map

---
+++ samples/src/main/java/home/saied/samples/gensampled/androidx/compose/runtime/samples/modulePathMap.kt
@@ -0,0 +1,26 @@
+package home.saied.samples.gensampled.androidx.compose.runtime.samples
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import home.saied.samples.*
+
+
+@ExperimentalComposeUiApi
+val modulePathMap: Map<String, String> = mapOf(
+    "material3" to "compose/material3/material3/samples/src/main/java/androidx/compose/material3/samples",
+    "ui-unit" to "compose/ui/ui-unit/samples/src/main/java/androidx/compose/ui/unit/samples",
+    "ui-graphics" to "compose/ui/ui-graphics/samples/src/main/java/androidx/compose/ui/graphics/samples",
+    "ui" to "compose/ui/ui/samples/src/main/java/androidx/compose/ui/samples",
+    "ui-text" to "compose/ui/ui-text/samples/src/main/java/androidx/compose/ui/text/samples",
+    "material-icons" to "compose/material/material-icons-core/samples/src/main/java/androidx/compose/material/icons/samples",
+    "material" to "compose/material/material/samples/src/main/java/androidx/compose/material/samples",
+    "foundation-layout" to "compose/foundation/foundation-layout/samples/src/main/java/androidx/compose/foundation/layout/samples",
+    "foundation" to "compose/foundation/foundation/samples/src/main/java/androidx/compose/foundation/samples",
+    "animation-core" to "compose/animation/animation-core/samples/src/main/java/androidx/compose/animation/core/samples",
+    "animation-graphics" to "compose/animation/animation-graphics/samples/src/main/java/androidx/compose/animation/graphics/samples",
+    "animation" to "compose/animation/animation/samples/src/main/java/androidx/compose/animation/samples",
+    "runtime-rxjava3" to "compose/runtime/runtime-rxjava3/samples/src/main/java/androidx/compose/runtime/rxjava3/samples",
+    "runtime-saveable" to "compose/runtime/runtime-saveable/samples/src/main/java/androidx/compose/runtime/saveable/samples",
+    "runtime-rxjava2" to "compose/runtime/runtime-rxjava2/samples/src/main/java/androidx/compose/runtime/rxjava2/samples",
+    "runtime" to "compose/runtime/runtime/samples/src/main/java/androidx/compose/runtime/samples",
+    "runtime-livedata" to "compose/runtime/runtime-livedata/samples/src/main/java/androidx/compose/runtime/livedata/samples",
+)
