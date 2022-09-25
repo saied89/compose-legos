2bc6c9b255f4da69d728aae6b2c4ca1d229bd861
saied89 <saiedmomen@gmail.com>
2022-09-26 01:50:28 +0330

    add action icon

---
+++ ideaplugin/src/main/java/icons/SdkIcons.kt
@@ -0,0 +1,10 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+
+object SdkIcons {
+    @JvmField
+    val ActionIcon: Icon = IconLoader.getIcon("/icons/compose_icon_action.svg", SdkIcons::class.java)
+}
