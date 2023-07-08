355342dbf441f46da5cdb6f9a7e20ea022c0fbf1
saied89 <saiedmomen@gmail.com>
2023-07-08 21:46:13 +0200

    generalize breadcrumbs

---
+++ app/src/main/java/home/saied/composesamples/ui/Breadcrumbs.kt
@@ -0,0 +1,79 @@
+package home.saied.composesamples.ui
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BreadCrumbsLazyRow(
+    overLapFraction: Float,
+    currentLocationName: String,
+    breadCrumbDetails: List<BreadCrumbDetail>
+) {
+    val fraction = if (overLapFraction > 0.01f) 1f else 0f
+    val appBarContainerColor by animateColorAsState(
+        targetValue = MaterialTheme.colorScheme.containerColor(fraction),
+        animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
+        label = "breadcrumbs color"
+    )
+    Surface(color = appBarContainerColor) {
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(bottom = 12.dp, start = 16.dp, end = 16.dp)
+        ) {
+            itemsIndexed(breadCrumbDetails) { index, item ->
+                Text(
+                    modifier = Modifier.clickable(onClick = item.onClick),
+                    text = item.name,
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Icon(
+                    imageVector = Icons.Filled.ChevronRight,
+                    contentDescription = null
+                )
+            }
+            item {
+                Text(
+                    text = currentLocationName,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        }
+    }
+}
+
+data class BreadCrumbDetail(val name: String, val onClick: () -> Unit)
+
+@Composable
+private fun ColorScheme.containerColor(colorTransitionFraction: Float): Color {
+    return lerp(
+        surface,
+        surfaceColorAtElevation(3.dp),
+        FastOutLinearInEasing.transform(colorTransitionFraction)
+    )
+}
--- app/src/main/java/home/saied/composesamples/ui/FileScreen.kt
+++ app/src/main/java/home/saied/composesamples/ui/FileScreen.kt
@@ -1,47 +1,27 @@
 package home.saied.composesamples.ui
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.Launch
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.contentColorFor
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -90,41 +70,11 @@ fun FileScreen(
                     },
                     scrollBehavior = scrollBehavior
                 )
-                val colorTransitionFraction = scrollBehavior.state.overlappedFraction
-                val fraction = if (colorTransitionFraction > 0.01f) 1f else 0f
-                val appBarContainerColor by animateColorAsState(
-                    targetValue = MaterialTheme.colorScheme.containerColor(fraction),
-                    animationSpec = spring(stiffness = Spring.StiffnessMediumLow)
+                BreadCrumbsLazyRow(
+                    overLapFraction = scrollBehavior.state.overlappedFraction,
+                    currentLocationName = sampleFile.name,
+                    breadCrumbDetails = listOf(BreadCrumbDetail(moduleName, onBackClick))
                 )
-                Surface(color = appBarContainerColor) {
-                    LazyRow(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        contentPadding = PaddingValues(bottom = 8.dp, start = 16.dp, end = 16.dp)
-                    ) {
-                        item {
-                            Text(
-                                modifier = Modifier.clickable(onClick = onBackClick),
-                                text = moduleName,
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                        }
-                        item {
-                            Icon(
-                                imageVector = Icons.Filled.ChevronRight,
-                                contentDescription = null
-                            )
-                        }
-                        item {
-                            Text(
-                                text = sampleFile.name,
-                                style = MaterialTheme.typography.bodySmall,
-                                color = MaterialTheme.colorScheme.tertiary
-                            )
-                        }
-                    }
-                }
             }
         }
     ) {
@@ -155,14 +105,4 @@ fun FileScreen(
             )
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-internal fun ColorScheme.containerColor(colorTransitionFraction: Float): Color {
-    return lerp(
-        surface,
-        surfaceColorAtElevation(3.dp),
-        FastOutLinearInEasing.transform(colorTransitionFraction)
-    )
 }
