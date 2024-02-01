6523c6c7fd5cb37de91e7b751456ba640b5c3bd2
saied89 <saiedmomen@gmail.com>
2024-02-01 20:30:43 +0100

    compose version 1.6.0

--- samples/src/main/java/home/saied/samples/samplesApi.kt
+++ samples/src/main/java/home/saied/samples/samplesApi.kt
@@ -1,6 +1,7 @@
 package home.saied.samples
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.ExperimentalComposeUiApi
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 val sampleModules = generatedSampleModules
